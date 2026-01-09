e07cd907c439e55c6e28603c29c0c808b734b01a
317moris <mori.souta.fu@st.spec.ed.jp>
2026-01-09 15:02:04 +0900

    feat: pandacssとchakraui presetのセットアップ

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,19 @@
-<h1>KSG</h1>
-<a
-	href="https://github.com/317moris/ksg-website-svelte"
-	target="_blank"
-	referrerpolicy="no-referrer"
->
-	GitHub
-</a>
+<script>
+	import { ExternalLink } from '@lucide/svelte';
+	import { container, heading, icon, link } from 'styled-system/recipes';
+</script>
+
+<main class={container()}>
+	<h1 class={heading()}>KSG</h1>
+	<a
+		href="https://github.com/317moris/ksg-website-svelte"
+		target="_blank"
+		referrerpolicy="no-referrer"
+		class={link()}
+	>
+		GitHub
+		<span aria-hidden="true" class={icon({ size: 'sm' })}>
+			<ExternalLink size="current" />
+		</span>
+	</a>
+</main>
