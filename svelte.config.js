278e437035fb37a03aae99e2e63ab060c1f94a87
森 蒼太 <mori.souta.fu@st.spec.ed.jp>
2026-02-28 00:33:17 +0900

    bunからpnpmに移行, VercelからCloudflare Workersに移行, 依存関係の更新 (#1)
    * chore: bun -> pnpm, 依存関係の更新
    
    * Vercel -> Cloudflare Workers
    
    * zedをeslint@10に一時的に対応させる

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,14 @@
-import adapter from '@sveltejs/adapter-vercel';
+// @ts-check
+import adapter from '@sveltejs/adapter-cloudflare';
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter(),
-		alias: {
-			'styled-system': './src/styled-system/*'
-		}
+		alias: { 'styled-system': './src/styled-system/*' }
 	}
 };
 
